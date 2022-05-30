a1ca63b3cd5015601e7c5f890708615d59c8704e
GeorgeGoodall <eorgegelliott@hotmail.co.uk>
2022-05-30 15:04:15 +0100

    mobile nav added

---
+++ script.js
@@ -0,0 +1,11 @@
+let mobileNavOpen = false;
+
+const openMobileNav = () => {
+    mobileNavOpen = true;
+    document.getElementById("mobileNav").style.display = "block";
+}
+
+const closeMobileNav = () => {
+    mobileNavOpen = true;
+    document.getElementById("mobileNav").style.display = "none";
+}
